aa5476adb28b442c209aaa6c6b339895a046196e
Misha <ipz211_bmm@student.ztu.edu.ua>
2022-01-28 21:03:25 +0200

    Created class LibaryUser inherited from class Human, also created constructor with parameters and method GetNumbAndDepos

---
+++ MainConsole/LibaryUser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainConsole
+{
+    class LibaryUser:Human
+    {
+        
+        public int NumberTicket { get; set;}
+        public DateTime DateOfIssue { get; set; }
+        public double SizeDeposit { get; set; }
+        public LibaryUser(string firstname, string lastname, DateTime dateOfBirthd,
+            int numberticket, DateTime dateIssue, double sizeDeposit) : base(firstname,
+                lastname, dateOfBirthd)
+        {
+            NumberTicket = numberticket;
+            DateOfIssue = dateIssue;
+            SizeDeposit = sizeDeposit;
+        }
+        public string GetNumbAndDepos()
+        {
+            string number = $"Номер білету:{NumberTicket.ToString()}" +
+                $"(pозмір Депозиту:{SizeDeposit.ToString()})";
+            
+            return number;
+        }
+
+    }
+}
