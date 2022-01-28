bbdebee75df5cc63d55616199f3cd3ee47919128
Misha <ipz211_bmm@student.ztu.edu.ua>
2022-01-28 20:49:56 +0200

    Created class Lecturer inherited from class Human and constructor with parameters and class GetPosAndDep

---
+++ MainConsole/Lecturer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainConsole
+{
+    class Lecturer:Human
+    {
+       
+        public string Position { get; set; }
+        public string Department { get; set; }
+        public string NameUniversuty { get; set; }
+        public Lecturer(string firstname, string lastname, DateTime dateofBirthd,
+            string position, string department, string university) :
+            base(firstname, lastname, dateofBirthd)
+        {
+            Position = position;
+            Department = department;
+            NameUniversuty = university;
+        }
+        public string GetPosAndDep()
+        {
+            string positions =$"Посaда{Position}(Кафедра:{Department})";
+
+            return positions;
+
+        }
+    }
+}
