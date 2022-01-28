b3c802d8b9f5a90df14cd53e14de553c419853d9
Misha <ipz211_bmm@student.ztu.edu.ua>
2022-01-28 19:35:29 +0200

    Created class ZNOpoint and constructor with parameters

---
+++ MainConsole/ZNOpoint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainConsole
+{
+    class ZNOpoint
+    {
+        public int FirstBalls { get; set; }
+        public int SecondBalls { get; set; }
+        public int ThirdBalls { get; set; }
+        public ZNOpoint(int firstBalls, int secondBalls,int thirdBalls)
+        {
+            FirstBalls = firstBalls;
+            SecondBalls = secondBalls;
+            ThirdBalls = thirdBalls;
+        }
+
+
+    }
+}
