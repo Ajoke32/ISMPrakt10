1de1d4f8c22cf2db847b8ab126ec78605a47dcf8
Misha <ipz211_bmm@student.ztu.edu.ua>
2022-01-28 19:29:58 +0200

    Created empty console app, created class Human and constructor with parameters and method GetAge

---
+++ MainConsole/Human.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainConsole
+{
+    class Human
+    {
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
+        public DateTime DateOfBirthd { get; set; }
+
+        public Human(string firstName, string lastName,DateTime dateOfBirthd)
+        {
+            FirstName = firstName;
+            Lastname = Lastname;
+            DateOfBirthd = dateOfBirthd;
+        }
+        public int GetAge()
+        {
+            TimeSpan age = DateTime.Now - DateOfBirthd;
+            return age.Days / 365;
+        }
+    }
+}
