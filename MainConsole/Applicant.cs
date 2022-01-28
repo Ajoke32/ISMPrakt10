750f9065621b0c8b1fe9ac890cf9956a78d600d5
Misha <ipz211_bmm@student.ztu.edu.ua>
2022-01-28 19:48:07 +0200

    From class Human inherited class Applicant is created, also created constructor with parametes

---
+++ MainConsole/Applicant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainConsole
+{
+    class Applicant:Human
+    {
+        public ZNOpoint Balls {get;set;}
+        public int EducationDocument { get; set;}
+        public string NameInstitution { get; set; }
+        public Applicant(string firstname,string lastname,DateTime dateOfBirthd,
+            ZNOpoint balls, int educationDoc, string nameInstitution)
+            :base(firstname,lastname,dateOfBirthd)
+        {
+            Balls = balls;
+            EducationDocument = educationDoc;
+            NameInstitution = nameInstitution;
+        }
+    }
+}
